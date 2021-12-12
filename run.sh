4caf6b4fb58d73fce4d11c32426fece72b846ee9
Angga R <nggdclxvi@protonmail.com>
2021-12-12 18:08:30 +0700

    Change repo url

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/AnggaR96s/kernel_xiaomi_joyeuse-1 -b S kernel --depth=5
+git clone https://github.com/AnggaR96s/kernel_xiaomi_joyeuse -b S kernel --depth=5
 cd kernel
 curl https://raw.githubusercontent.com/ARCHSTNC/DroneCI/main/build.sh | bash
