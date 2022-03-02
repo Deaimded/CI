d595949d4fead82e711269c9ccf7b30c32d064af
Angga R <nggdclxvi@protonmail.com>
2022-03-02 23:09:56 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -130,7 +130,7 @@ DATE=$(TZ=Asia/Jakarta date +"%F_%H-%M-%S")
  clone() {
 	echo " "
 	msg "|| Cloning Clang ||"
-	git clone --depth 1 --no-single-branch https://gitlab.com/AnggaR96s/clang-gengkapak clang
+	git clone --depth 1 https://scm.osdn.net/gitroot/gengkapak/clang-GengKapak.git -b main clang
 		# Toolchain Directory defaults to clang-llvm
 	TC_DIR=$KERNEL_DIR/clang
 
