87668dd4e99f5dc10eb111c10002c914e626f655
Deaimded <107251898+Deaimded@users.noreply.github.com>
2023-01-10 18:26:07 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+echo "$CREDENTIALS" > ~/.git-credentials
+
 git clone $REPO kernel --depth=5
 cd kernel
-curl https://raw.githubusercontent.com/ARCHSTNC/DroneCI/main/build.sh | bash
+curl https://raw.githubusercontent.com/Deaimded/CI/main/build.sh | bash
